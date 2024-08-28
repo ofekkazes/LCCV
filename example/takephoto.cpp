f05907909d61c7c25ba2ac2938fef1264bfa9f9a
Ofek Kazes <42179756+ofekkazes@users.noreply.github.com>
2024-08-28 15:15:36 +0300

    Update takephoto.cpp

--- example/takephoto.cpp
+++ example/takephoto.cpp
@@ -1,4 +1,5 @@
 #include <lccv.hpp>
+#include <libcamera_app_options.hpp>
 #include <opencv2/opencv.hpp>
 
 int main()
@@ -7,11 +8,12 @@ int main()
     lccv::PiCamera cam;
     //cam.options->width=4056;
     //cam.options->height=3040;
-    cam.options->photo_width=2028;
-    cam.options->photo_height=1520;
+    cam.options->photo_width=1920;
+    cam.options->photo_height=1080;
     cam.options->verbose=true;
     cam.options->gain = 2.0f;
     cam.options->shutter = 0.1f;
+    cam.options->setWhiteBalance(WB_DAYLIGHT);
     cam.capturePhoto(image);
     cv::rotate(image, image, cv::ROTATE_90_CLOCKWISE);
     cv::Mat denoised;
